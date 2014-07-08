238d025fb5f18548f3b98fd109c9e2bc203936c5
Evan Harding <eaharding3t@hotmail.com>
2014-07-08 10:30:39 -0400

    more phantomRun Fixes

--- my_webapp/recipes/phantomRun.rb
+++ my_webapp/recipes/phantomRun.rb
@@ -1,5 +1,5 @@
 filename = "loadTest.js"
 execute 'execute_phantom' do
 	cwd '/home/ec2-user'
-	command 'phantomjs loadTest.js [node[:values][:numOfScriptExecutesRequested] node[:value][:adressToTest] node[:value][:queriesToRun] node[:values][:requestInterval] node[:values][:commands]'
+	command 'phantomjs loadTest.js node[:values][:numOfScriptExecutesRequested] node[:values][:adressToTest] node[:values][:queriesToRun] node[:values][:requestInterval] node[:values][:commands]'
 end
