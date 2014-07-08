a6807d038154d6539e96b89593b1fb2050bf735d
Evan Harding <eaharding3t@hotmail.com>
2014-07-08 13:40:19 -0400

    more updates

--- my_webapp/recipes/testRun.rb
+++ my_webapp/recipes/testRun.rb
@@ -1,4 +1,4 @@
 execute 'execute_phantom' do
 	cwd '/home/ec2-user'
-	command 'phantomjs loadTest.js node[:values][:numOfScriptExecutesRequested] node[:values][:adressToTest] 1 node[:values][:requestInterval] node[:values][:commands]'
+	command 'phantomjs loadTest.js node[:values][:numOfScriptExecutesRequested] node[:values][:adressToTest] node[:values][:numOfScriptExecutesRequested] node[:values][:requestInterval] '
 end
