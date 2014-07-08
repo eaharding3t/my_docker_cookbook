53c87951990551d2e32fa9a754d5795c24766863
Evan Harding <eaharding3t@hotmail.com>
2014-07-08 14:57:10 -0400

    more updates

--- my_webapp/recipes/testRun.rb
+++ my_webapp/recipes/testRun.rb
@@ -1,4 +1,4 @@
 execute 'execute_phantom' do
 	cwd '/home/ec2-user'
-	command 'phantomjs loadTest.js node[:values][:numOfScriptExecutesRequested] node[:values][:adressToTest] node[:values][:requestIn] node[:values][:requestIn] "{\"0\": {\"executionType\": \"assign\",\"function\": \"value\",\"data\": true},\"1\": {\"executionType\": \"headlessExecuteNoParams\",\"function\": \"onLoading\",\"data\": \"\"}}"' 
+	command 'phantomjs loadTest.js node[:values][:numOfScriptExecutesRequested] node[:values][:adressToTest] 100 node[:values][:requestIn] "{\"0\": {\"executionType\": \"assign\",\"function\": \"value\",\"data\": true},\"1\": {\"executionType\": \"headlessExecuteNoParams\",\"function\": \"onLoading\",\"data\": \"\"}}"' 
 end
