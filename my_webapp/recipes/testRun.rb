ba4dc9cf7ff0da325e6f589e27bee95111945974
Evan Harding <eaharding3t@hotmail.com>
2014-07-08 11:01:18 -0400

    testing

--- my_webapp/recipes/testRun.rb
+++ my_webapp/recipes/testRun.rb
@@ -1,4 +1,4 @@
 execute 'execute_phantom' do
 	cwd '/home/ec2-user'
-	command 'echo node[:values][:commands]'
+	command 'phantomjs loadTest.js 1 "http://basicpoc.elasticbeanstalk.com/start/" 1 200 "{\"0\": {\"executionType\": \"assign\",\"function\": \"value\",\"data\": true},\"1\": {\"executionType\": \"headlessExecuteNoParams\",\"function\": \"onLoading\",\"data\": \"\"}}"' 
 end
