819ad45482fa4d181dbaf5907e8a7217bdbfee27
Evan Harding <eaharding3t@hotmail.com>
2014-07-09 13:16:25 -0400

    reverts

--- my_webapp/recipes/phantomRun.rb
+++ my_webapp/recipes/phantomRun.rb
@@ -1,4 +1,4 @@
 execute 'execute_loadTest' do
 	cwd '/local/'
-	command 'sudo phantomjs loadTest.js '+node[:adress]+' '+node[:repeat]+' 200 "{\"0\": {\"executionType\": \"assign\",\"function\": \"value\",\"data\": true},\"1\": {\"executionType\": \"headlessExecuteNoParams\",\"function\": \"onLoading\",\"data\": \"\"}}"'
+	command 'sudo phantomjs loadTest.js '+node[:adress]+' '+node[:repeat]+' '+node[:spacing]+' '+node[:jsToRun]
 end
