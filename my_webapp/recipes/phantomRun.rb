9c42f65f2ef7c454d4e36005d5499321ce34212f
Evan Harding <eaharding3t@hotmail.com>
2014-07-09 09:26:41 -0400

    more updates

--- my_webapp/recipes/phantomRun.rb
+++ my_webapp/recipes/phantomRun.rb
@@ -1,5 +1,5 @@
 filename = "loadTest.js"
 execute 'execute_loadTest' do
 	cwd '/local/'
-	command 'sudo phantomjs loadTest.js node[:adress] 20 node[:spacing] node[:jsToRun]'
+	command 'sudo phantomjs loadTest.js node[:adress] 20 node[:spacing] "{\"0\": {\"executionType\": \"assign\",\"function\": \"value\",\"data\": true},\"1\": {\"executionType\": \"headlessExecuteNoParams\",\"function\": \"onLoading\",\"data\": \"\"}}"'
 end
