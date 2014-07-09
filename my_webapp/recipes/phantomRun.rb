d4814b28813f1957e5bc600d13de50a1692fd704
Evan Harding <eaharding3t@hotmail.com>
2014-07-09 10:51:18 -0400

    reverts

--- my_webapp/recipes/phantomRun.rb
+++ my_webapp/recipes/phantomRun.rb
@@ -1,5 +1,4 @@
-filename = "loadTest.js"
 execute 'execute_loadTest' do
 	cwd '/local/'
-	command 'sudo phantomjs loadTest.js node[:adress] 20 node[:spacing] "{\"0\": {\"executionType\": \"assign\",\"function\": \"value\",\"data\": true},\"1\": {\"executionType\": \"headlessExecuteNoParams\",\"function\": \"onLoading\",\"data\": \"\"}}"'
+	command 'sudo phantomjs loadTest.js "http://basicpoc.elasticbeanstalk.com/start/" 1 200 "{\"0\": {\"executionType\": \"assign\",\"function\": \"value\",\"data\": true},\"1\": {\"executionType\": \"headlessExecuteNoParams\",\"function\": \"onLoading\",\"data\": \"\"}}"'
 end
